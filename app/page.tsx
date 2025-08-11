7b55df61111aa7345b14969be0f88964753e2fad
Hugo-walando <hugowalando@gmail.com>
2025-08-11 23:49:25 +0200

    feat:add style, import InputForm & Loader

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,25 @@ import InputForm from './components/InputForm';
 import { useEffect, useState } from 'react';
 import { AnimatePresence } from 'motion/react';
 import LoadingScreen from './components/LoadingScreen';
+import { useGenerationStore } from './store/useGenerationsStore';
 
 export default function HomePage() {
   const [isLoading, setIsLoading] = useState(false);
   const [isReady, setIsReady] = useState(false);
   const router = useRouter();
+  const createGeneration = useGenerationStore(
+    (state) => state.createGeneration,
+  );
 
   const handleIdeaSubmit = (idea: string) => {
-    setIsLoading(true);
+    if (!idea.trim()) return;
 
-    // Simule la fin du traitement IA après un temps aléatoire (3-5s)
-    const fakeTime = Math.floor(Math.random() * 2000) + 3000; // 3 à 5 sec
+    // On sauvegarde l'idée dans le store
+    createGeneration(idea);
+
+    // On simule le chargement
+    setIsLoading(true);
+    const fakeTime = Math.floor(Math.random() * 2000) + 3000;
     setTimeout(() => {
       setIsReady(true);
     }, fakeTime);
@@ -23,18 +31,17 @@ export default function HomePage() {
 
   useEffect(() => {
     if (isReady) {
-      // Une fois prêt, on attend un petit délai pour finir l'anim et on redirige
       setTimeout(() => {
         router.push(`/result`);
-      }, 800); // temps pour finir l'anim
+      }, 800);
     }
   }, [isReady, router]);
   return (
     <main className='max-w-7xl mx-auto p-6'>
-      <section className='flex flex-col items-center pt-10 gap-4'>
-        <div className='absolute top-5 right-5 w-[500px] h-[500px] bg-purple-glow blur-3xl -z-10'></div>
-        <div className='absolute top-40 left-5 w-[500px] h-[500px] bg-purple-glow blur-3xl -z-10'></div>
-        <div className='absolute top-[-100px] left-80 w-[300px] h-[300px] bg-purple-glow blur-3xl -z-10'></div>
+      <section className='flex flex-col items-center text-center pt-10 gap-6'>
+        <div className='absolute top-5 right-5 w-[500px] h-[300px] bg-purple-glow blur-3xl -z-10'></div>
+        <div className='absolute top-40 left-5 w-[500px] h-[300px] bg-purple-glow blur-3xl -z-10'></div>
+        <div className='absolute top-[-100px] left-80 w-[200px] h-[200px] bg-purple-glow blur-3xl -z-10'></div>
         <h1 className='text-7xl font-semibold mb-6 '>
           <span className='bg-gradient-to-r from-purple-300 from-5% to-20% to-slate-50 bg-clip-text text-transparent'>
             Transformez vos idées en <br />
