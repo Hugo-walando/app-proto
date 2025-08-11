7cfa3bb7c10f7fca77be8be50b9ae89a4eced767
Hugo-walando <hugowalando@gmail.com>
2025-08-11 23:49:55 +0200

    feat:create InputForm comp

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useState } from 'react';
+import { WandSparkles } from 'lucide-react';
+import { useState, useRef, useEffect } from 'react';
 
 interface InputFormProps {
   onSubmit: (idea: string) => void;
@@ -8,26 +9,39 @@ interface InputFormProps {
 
 export default function InputForm({ onSubmit }: InputFormProps) {
   const [idea, setIdea] = useState('');
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  // Redimensionner automatiquement le textarea
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height =
+        textareaRef.current.scrollHeight + 'px';
+    }
+  }, [idea]);
 
   const handleSubmit = () => {
     if (!idea.trim()) return;
     onSubmit(idea);
+    setIdea('');
   };
 
   return (
-    <div className='w-full max-w-md'>
+    <div className='w-full max-w-3xl relative flex'>
       <textarea
+        ref={textareaRef}
         value={idea}
         onChange={(e) => setIdea(e.target.value)}
-        placeholder='Exemple : une app de recettes qui suggère des plats selon les ingrédients...'
-        className='w-full p-4 rounded-xl bg-gray-800 text-white border border-gray-700 focus:outline-none focus:border-purple-500 resize-none'
-        rows={4}
+        placeholder='Décrivez votre idée...'
+        rows={1}
+        className='w-full rounded-4xl p-6 pr-40 bg-white/5 backdrop-blur-md text-white border border-gray-700 focus:outline-none focus:border-purple-500 resize-none overflow-hidden'
       />
       <button
         onClick={handleSubmit}
-        className='mt-4 w-full py-3 bg-purple-600 hover:bg-purple-700 rounded-xl font-medium transition-colors'
+        className='absolute flex items-center gap-2 right-2 top-2 px-8 py-4 rounded-full bg-gradient-to-r from-purple to-purple-dark font-medium transition-colors hover:cursor-pointer'
       >
-        Générer ma maquette
+        Générer
+        <WandSparkles className='w-5 h-5 ' />
       </button>
     </div>
   );
